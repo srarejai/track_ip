21c0aaf8c354066f87c769380c37d2414a64d53e
Sitharaam Jayakumar <sr.jai.estatemate@gmail.com>
2018-08-31 09:55:37 +0530

    latest track_ip with IpInfoDb api

--- src/Demo.php
+++ src/Demo.php
@@ -9,22 +9,39 @@ class Demo
 
     /**** Function to geocode ip address. Accepts ip as parameter and returns geocoded details as an array ****/
 
-    public function getGeoLocation($ip)
+    public function getGeoLocation($ip, $apikey, $format)
     {
+        /**** Check format requested ****/
+
+        if($format !== 'raw' && $format !== 'xml' && $format !== 'json')
+        {
+           $result = [];
+           $result['Error'] = '101';
+           $result['Description'] = 'Invalid Result Format Requested';
+           return jscon_encode($result);
+        }
+
         /**** Check if passed ip is valid ****/
 
 	      $valid = preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/', $ip);
 
 	      if($valid)
 	      {
+          /**** Construct IpInfoDB ****/
+
+          $apiurl = "http://api.ipinfodb.com/v3/ip-city/?key=".$apikey."&ip=".$ip."&format=".$format;
+
           /**** geo code using extreme-ip-lookup service and store in array ****/
-	        $geo = json_decode(file_get_contents("http://extreme-ip-lookup.com/json/".$ip), true);
+	        $geo = file_get_contents($apiurl);
 
 	        /**** return geooded array ****/
 	        return $geo;
 	      }
         else {
-            return false;
+          $result = [];
+          $result['Error'] = '102';
+          $result['Description'] = 'Invalid Ip Format';
+          return jscon_encode($result);
         }
     }
 }
